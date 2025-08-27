5e9a68f3f3562954a53c327b1b1ab3884a80efcd
yusratmiah <yusratmiah@vt.edu>
2025-08-27 13:58:48 -0400

    Update PHP files

---
+++ l.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+echo "<title>This is my file named, l.php</title>";
+?>
+</body>
+</html>
---
+++ y.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+echo "<title>This is my file named, y.php</title>";
+?>
+</body>
+</html>
